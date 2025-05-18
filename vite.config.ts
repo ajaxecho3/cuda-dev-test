64150aa22474eca364449f608d00e8cf8fa177db
ajaxecho3 <ajaxecho3@gmail.com>
2025-05-19 02:09:12 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/cuda-dev-test",
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
